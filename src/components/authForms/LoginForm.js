6a9ddcac565721e2f20fc8596345db4e2fbee14f
Uche Ngadi <uchengadi@yahoo.com>
2022-10-22 02:20:35 +0100

    loginform script refactoring

--- src/components/authForms/LoginForm.js
+++ src/components/authForms/LoginForm.js
@@ -5,7 +5,7 @@ import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import { makeStyles, useTheme } from "@material-ui/core/styles";
 import Button from "@material-ui/core/Button";
-import background from "./../../logistic_assets/cover_image_1.png";
+import background from "./../../assets/image/cover/cover_image.png";
 
 import { TextField } from "@material-ui/core";
 
